b43bcca9cdbf5473d75ee1f130aa263f8229f765
Weite Dai <weitedai.dev@gmail.com>
2022-09-26 01:40:36 +0000

    style: reformat movie sql in db migration v1 (pull request #3)
    *Merged in feature/MOVIEW-24-refactor-movie-sql-format
    
    Approved-by: Zora
    Approved-by: Jacqueline Zhao

--- src/main/resources/db/migration/V1__Create_init_tables.sql
+++ src/main/resources/db/migration/V1__Create_init_tables.sql
@@ -6,14 +6,14 @@ CREATE TABLE genre
 
 CREATE TABLE movie
 (
-    id           BIGSERIAL PRIMARY KEY,
-    name         VARCHAR(255)             NOT NULL,
-    genre_id     INTEGER                  NOT NULL REFERENCES genre (id),
-    year         INTEGER,
-    director     VARCHAR(128),
-    actor        VARCHAR(255),
-    description  VARCHAR(1000),
-    poster_img_url   VARCHAR(255),
-    created_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_time TIMESTAMP WITH TIME ZONE NOT NULL
+    id             BIGSERIAL PRIMARY KEY,
+    name           VARCHAR(255)             NOT NULL,
+    genre_id       INTEGER                  NOT NULL REFERENCES genre (id),
+    year           INTEGER,
+    director       VARCHAR(128),
+    actor          VARCHAR(255),
+    description    VARCHAR(1000),
+    poster_img_url VARCHAR(255),
+    created_time   TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_time   TIMESTAMP WITH TIME ZONE NOT NULL
 );
