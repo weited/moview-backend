26db59b696edf26adc4829931d15b9a5523a0e2c
alex zhao <alexzhao562@gmail.com>
2022-10-25 00:47:26 +0000

    feat: add init data for post and tag table (pull request #23)

---
+++ src/main/resources/db/migration/V5__Add_post_and_tag_data.sql
@@ -0,0 +1,94 @@
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('Melbourne','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('Sydney','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('Fiction','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('Comedy','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('The Avengers','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO tag(tag_name,created_time,updated_time)
+VALUES('StarCraft','2022-10-24 20:19:25 +00:00', '2022-10-24 20:19:25 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('The people no one imagines do the things no one can imagine',
+'bring WWII to an end, saving millions of lives, ending Nazi Germany, and the beginning of computer sciences that will change our world an action adventure In it`s own. Alan Turing was never even given the right to exist the way he wanted to live his own life, treated like a second class citizen, and committed suicide. We`ll never know what one of the greatest minds could have achieved because of the ignorance of the British government and their laws against homosexuality. Today Turing would have been able to thrive in a more tolerant world, and have the support of many. He was the man needed at the time, but also way to far ahead of the times for so many to even comprehend. He helped save the world, yet the Brits not only helped put him in his grave, but ignored his work and existence for 50 years after his death at 41. This movie is for those wanting to know about people who change our world, people who go so far and above anything most people could even imagine, and with minds that very few could even begin to understand. Every kid should learn about Turing, what he achieved, and how not to treat people based on gender, religion, race, or sexual preference.  This man was a gift to the world, and the world treated him like dirt in return for all he gave us. I`m so happy to see a film that gives some justice to Turing`s life, and give him at least some acknowledgement for what he did for this world that maybe only a handful of people could ever have done. I can`t praise the importance of this film, or how well Cumberbatch and Knightley played the parts of Turing and Clark. Cumberbatch proves time and time again he`s a great actor, and Kiera, she`s always spot on. Two of the best, in a world class film, that all should see.  `Sometimes it`s the people no one imagines anything of, who do the things no one can imagine` My God how true.',
+1, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('A potent wartime drama and tragic human rights tale',
+'A potent wartime drama and tragic human rights tale, The Imitation Game puts Benedict Cumberbatch in another role as an eccentric genius who looks at the world from a unique perspective. After stunning millions of fans and maintaining cultish following of `Cumberbitches` with his turn as the modern Sherlock Holmes on BBC`s popular Sherlock television series, he realizes an award-worthy portrayal as Alan Turing, the ostensible father of artificial intelligence who cracked Germany`s Enigma code machine and in turn helped win World War II. Beyond building the first computer, Turing was also a victim of Britain`s cruel Labouchere Amendment of 1885, which considered homosexuals like himself indecent and therefore subject to criminal reeducation. This fascinating and many-textured subject matter comes to life as certain Oscar bait in the film from Norwegian director Morten Tyldum (Headhunters) and distributors at The Weinstein Company. This early review comes after a screening at the Twin Cities Film Fest, where the audience received it with much applause and enthusiasm.',
+1, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('The beginning of computer sciences during World War II',
+'Based on the life of the brilliant British Mathematician, Alan Turing, the movie features his efforts to break the Nazi Enigma Code through a machine which eventually paved the way for modern day computers.
+        It requires something unimaginable to work out the 159 million million million possible ways to break the Enigma Code and save lives, which would otherwise become casualty figures in a great war – and the movie has beautifully captured this.
+        A dazzling portrayal of a genius played by Benedict Cumberbatch, it`s perhaps one of the best biopics one could watch. Keira Knightley played her role exceptionally well as Joan Clarke. Her acting precisely depicts how a bright woman might have felt in that era, when sexism was rampant and conservatism required for a woman to stay at home. She was able to portray all the problems that women came across in fields which were largely dominated by men, such as cryptography.
+        An attractive and powerful screenplay by Graham Moore, it proved to be an incredible delight for the ears and brain. The following dialogue is my favorite, because of how tremendous and emotional it is. It comes at the end of the movie, when Joan tells Alan:
+        `Do you know, this morning I was on a train that went through a city that wouldn`t exist if it wasn`t for you. I bought a ticket from a man who would likely be dead if it wasn`t for you. I read up, on my work, a whole field of scientific inquiry that only exists because of you. Now, if you wish you could have been normal… I can promise you I do not. The world is an infinitely better place precisely because you weren`t (normal).`
+',
+2, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('Strong Story & Strong Casting',
+'        This movie is for all the people who loves mystery, mathematics, and true events.
+        Needless to say, this is the award winning movie and still keeps you on toes when watched. I have personally cherished this movie 5 times.
+        Genre: Intelligent · Strong Acting · Inspirational Characters · Realistic · Inspiring · Intense · Touching · Captivating · Thought Provoking.
+        This movie is based on the true events during the World War II between Britsh and Nazis (Hitler).
+        This movie has truly out performed the event in many ways. The first is with the strong casting. Benedict Cumberbatch as ALAN TURING our lead character in the movie. We all know this actor has reached the epitome in delivering his serious faced skills. We`ve seen him in Sherlock Holmes doing the method acting.
+        This movie rolls around numbers, alan turing, his madness for solving problems and his problems with his fellow mates.
+',
+3, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('Your will love all the meaningful quotes',
+'I have watched this awesome movie four times! The characters have become like old friends, thanks to the fantastic crew and the stellar actors.
+
+        Interspersed as it is with meaningful quotes, which in their simplicity, leave you thinking about their inherent common sense and aptness, as when Alan Turing said, `sometimes we can`t do what feels good; we have to do what is logical.` This, after they had broken the Enigma Code and successfully mapped the location of all British warships, convoys, and German U-boats in the North Atlantic told his team members that they couldn`t just go on a saving spree lest the Germans got wind of their breakthrough, change the settings of the Enigma machine, and bring about three years of hard work to nothing.
+        Or Joan`s advice to Alan on the importance of interpersonal relationships when she said `if you really want to solve your puzzle, then you are going to need all the help you can get, and they (his team members) are not going to help you if they do not like you`, and how Alan made a little more effort after this. But based on what we now know today, he may have been on the Autism spectrum seeing as he found it difficult to understand nuanced speech and interactions, comparing conversations, as he once did, to ciphers; thus lacking the basic skills needed for social connections (and which sadly hurt him a lot because he was misunderstood).
+',
+1, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('The people no one imagines do the things no one can imagine',
+'    One of the best movies I`ve ever seen. No, there`s no big explosions, no gun fight scenes, no action heros, and no big finally that leaves you hanging for the next film release, unless you consider the genius behind Turing that helped bring WWII to an end, saving millions of lives, ending Nazi Germany, and the beginning of computer sciences that will change our world an action adventure In it`s own. Alan Turing was never even given the right to exist the way he wanted to live his own life, treated like a second class citizen, and committed suicide. We`ll never know what one of the greatest minds could have achieved because of the ignorance of the British government and their laws against homosexuality. Today Turing would have been able to thrive in a more tolerant world,',
+2, 17, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('·Hope is a good thing probably best of all & good thing never dies .',
+'This line from this movie shows the actual truth what humanity need most . · The Shawshank redemption · is based upon legendary stephen king·s novel which represents very truth of life never lose hope because it probably best of all . Great direction by frank darabondt , impeccable script , screenplay , towering performances by cast made this movie greatest movie ever made . Duffrian·s relentless effort to get away from prison life stands  him alone from others , life in prison is totally different one just have to confront his /her fear trauma , one has  to tackle with them else life will be nothing less than hell , also keep your mind busy as  mind itself can be most troubled thing in prison life if it remained unattended . Casting is another great thing about this movie , characters of Tim Robbins & Morgan Freeman have been crafted by director so perfectly that they have become great source of inspiration for all. I personally belive this movie is life learning lesson  , everyone must need to watch this movie and understand how difficult traumatic life could be ,one of the  the scene where one of the inmate committed suicide when he was released from prison and allowed to live a common man life as it was tough for him to adjust with society like he has become accustomed to prison life , that harrowing reality will shock you how traumatic life could be . This great movie is absolutely  an  evershing gem of world cinema . Don·t miss this great movie it·s a life time experience of a kind you won·t ever forget.',
+3, 2, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('·Inception·: The stuff that dreams are made of',
+'·I hate my dreams. They·re so ... infantile.· Heaven knows what artist Laurie Anderson would make of Christopher Nolan·s first film since ·The Dark Knight,· but if audiences don·t go for it, we·re more likely to hear a different complaint: It·s just too darned complex.
+        A spectacular fantasy thriller based on Nolan·s own original screenplay, ·Inception· is the smartest CGI head-trip since ·The Matrix.· The premise is so out-there, it requires not just a leap of faith but also too much screen time to explain, so do yourself a favor and pay attention.
+
+        Leonardo DiCaprio is Cobb, an ·excavator,· who digs around in people·s subconscious while they·re catching some zzzzs. This act of infiltration is ·not strictly legal·; the motive is not therapeutic but espionage and theft.
+',
+1, 45, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
+
+INSERT INTO post(title, contents, author_id, movie_id, created_time, updated_time)
+VALUES('Even after 50 years, The Godfather is still the Don Corleone of movies.',
+'Even half a century since its release, it shouldn·t be difficult to convince you why The Godfather is worth catching this weekend for its anniversary.
+
+It·s The Godfather – the title itself is practically a synonym for good cinema. Adapted from Mario Puzo·s novel, Francis Ford Coppola had to fight to get his vision on screen.
+
+He wasn·t the first choice of director, and had to battle studio Paramount to cast Marlon Brando, by then seen as a washed-up shadow of his 1950s glory. Coppola would be vindicated, however, as he crafted the saga of the Corleone family, led by the revered Don Vito Corleone (Brando).
+
+From the infamous opening scene, where Vito grants favours on his daughter·s wedding day, the nerve-shredding moment where Michael commits murder in a restaurant, stiffly following the advice given by family associate Clemenza (Richard S. Castellano). The latter scene in particular is a symphony of filmmaking, where visual and sound combine to tell a marvellous story. As for great performances, take your pick.
+
+Brando became a cinema icon thanks to Don Corleone, a role that would trigger a comeback for the actor. With a muffled voice and graceful demeanour, he would become the blueprint for all mafia movies to this day.
+
+Decades before his ·Hoo-Haa” scenery chewing, Pacino·s performance is among his most subtle, showing someone corrupted by both power and responsibility. Providing the fire to his ice is James Caan as Sonny, the hot-tempered brother who would be Don if it weren·t for his compulsive nature.
+
+Despite only being her second feature role, Diane Keaton would set the standard for a glittering career to come as Kay, Michael·s fiancé who would represent his last hope for salvation The film would be bettered by its sequel, but The Godfather remains an absolute pleasure to witness on the big screen.
+',
+2, 20, '2022-10-24 20:36:55 +00:00', '2022-10-24 20:36:55 +00:00');
