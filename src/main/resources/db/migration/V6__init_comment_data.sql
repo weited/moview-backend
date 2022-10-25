f36e30a5daa4624aeed89bfce1419204ef99c290
Jacqueline Zhao <dzhao6@utas.edu.au>
2022-10-25 13:42:07 +1100

    feat(comment): add init comments data

---
+++ src/main/resources/db/migration/V6__init_comment_data.sql
@@ -0,0 +1,31 @@
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('I have just seen the film, well about 35mins of it so far, I had to stop and google the quote. I love it. It rings so true about what life throws at us and what we can throw at life. I shall read more of your blog, first I must finish watching the film. Courage is indeed contagious. Have an amazing day!',
+null, 1, 3, '2022-10-24 20:40:55+00', '2022-10-24 20:40:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('Yes, I also googled the quote while watching the movie. Amazing quotable quote',
+1, 1, 2, '2022-10-24 20:40:55+00', '2022-10-24 20:40:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('I too saw the movie “The Imitation Game” and had to Google the quote as this is an amazing line that stands out above all the rest. I agree whole hardheartedly, never pre-judge and never discount or underestimate your own wealth. Certainly I was not the only one to notice this and I will surly be back to read more of your review. Good day all.',
+null, 1, 2, '2022-10-24 20:44:55+00', '2022-10-24 20:44:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('The line is also in Tim Burton`s Alice in Wonderland.',
+3, 1, 4, '2022-10-24 21:40:55+00', '2022-10-24 21:40:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('Queen Elizabeth granted Turing a posthumous pardon for his so-called crimes in 2013, and The Imitation Game does a fine job of making Turings life story accessible for mass consumption—arguably to a fault, since his sexuality is more a concept than a reality shown onscreen, with any physical realization of Turings homosexuality conspicuously avoided.',
+null, 2, 3, '2022-10-24 20:40:55+00', '2022-10-24 20:40:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('Noted objections about the standard handling and thematic transparency wont prevent anyone from savoring the engaging subject, nor will they spoil any overall appreciation for its narrative rewards and impressive central performance.',
+5, 2, 2, '2022-10-24 20:40:55+00', '2022-10-24 20:40:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('Cumberbatchs portrayal is the films brilliant centerpiece, filled with arrogance and vulnerability, and he is wonderfully textured throughout, particularly in later scenes.',
+null, 2, 4, '2022-10-24 20:44:55+00', '2022-10-24 20:44:55+00');
+
+INSERT INTO comment(text, parent_id, post_id, user_id, created_time, updated_time)
+VALUES('Though he may seem at risk of becoming typecast as such maligned, eccentric masterminds as Sherlock or his upcoming role in Marvel’s Dr. Strange, the actor manages to incorporate an impressive degree of variation from one such role to the next.',
+7, 2, 1, '2022-10-24 21:40:55+00', '2022-10-24 21:40:55+00');
